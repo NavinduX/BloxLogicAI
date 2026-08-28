f512cf8455ffe6e8b572d59f1e631ca4cc6a1acb
22730 <22730@example.com>
2026-08-28 05:34:25 +0530

    Update Data_load

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -51,7 +51,7 @@ ANNUAL_EXPORT_MT = {2021: 286016, 2022: 250191, 2023: 241912}
 
 
 # ---------------------------------------------------------------------------
-# Export: long monthly history (2011-2026) -> single national total series 
+# Export: long monthly history (2011-2026) -> single national total series
 # ---------------------------------------------------------------------------
 def load_export_monthly() -> pd.DataFrame:
     """National monthly tea export volume (MT) from the SLTB customs export file.
